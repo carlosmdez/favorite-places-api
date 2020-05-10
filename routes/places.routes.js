0296d4bf1359bbedbfda26ea0a4b274976eaaeaa
Carlos Méndez <carlos.mdez@outlook.com>
2020-05-10 17:52:32 -0500

    feat: Add places routes

---
+++ routes/places.routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const {
+  getPlaceById,
+  getPlacesByUserId,
+  createPlace
+} = require('../controllers/places-controller')
+
+const router = express.Router()
+
+router.get('/:placeId', getPlaceById)
+router.get('/user/:userId', getPlacesByUserId)
+router.post('/', createPlace)
+
+module.exports = router
